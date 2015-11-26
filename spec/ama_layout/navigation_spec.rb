224a74b6c6ceee3d76303dd5f970c8ea37123ea3
Ruben Estevez <ruben.a.estevez@gmail.com>
2015-11-26 15:02:26 -0700

    Add missing links on drivered subnav
    * added test for subnav for future changes.

--- spec/ama_layout/navigation_spec.rb
+++ spec/ama_layout/navigation_spec.rb
@@ -45,6 +45,20 @@ describe AmaLayout::Navigation do
         expect(subject.items[5].link).to eq "#"
         expect(subject.items[5].icon).to eq "fa-cogs"
       end
+
+      context "subnavs" do
+        context "driver education" do
+          let(:driver_education_subnav) { subject.items[2].sub_nav }
+
+          it "return the subnav items" do
+            expect(driver_education_subnav[0].text).to eq "Driver Education Overview"
+            expect(driver_education_subnav[0].link).to eq "#{driveredonline_site}/"
+
+            expect(driver_education_subnav[1].text).to eq "New Driver Online Program"
+            expect(driver_education_subnav[1].link).to eq "#{driveredonline_site}/dashboard"
+          end
+        end
+      end
     end
 
     context "non-member" do
