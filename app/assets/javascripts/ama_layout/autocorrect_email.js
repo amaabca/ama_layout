d8c02788fe34ec157fc562e5040c201d7cb4d9f0
Darko Dosenovic <darko.dosenovic@ama.ab.ca>
2016-01-22 08:57:41 -0700

    Updating code to use JS only in order to warn user about misspelled email domains. No cdoe changes in apps are required.

--- app/assets/javascripts/ama_layout/autocorrect_email.js
+++ app/assets/javascripts/ama_layout/autocorrect_email.js
@@ -1,44 +1,45 @@
-var domains = ['msn.com', 'bellsouth.net', 'telus.net', 'comcast.net', 'optusnet.com.au', 'earthlink.net', 'qq.com',
+window.onload = function(){
+  var domains = ['msn.com', 'bellsouth.net', 'telus.net', 'comcast.net', 'optusnet.com.au', 'earthlink.net', 'qq.com',
                 'sky.com', 'icloud.com', 'mac.com', 'sympatico.ca', 'googlemail.com', 'att.net', 'xtra.co.nz', 'web.de',
                 'cox.net', 'gmail.com', 'ymail.com', 'aim.com', 'rogers.com', 'verizon.net', 'rocketmail.com',
                 'google.com', 'optonline.net', 'sbcglobal.net', 'aol.com', 'me.com', 'btinternet.com', 'charter.net',
                 'shaw.ca', 'xplore.com', 'abnorth.com', 'pentnet.net', 'canadasurf.net', "ama.ab.ca"];
 
-var secondLevelDomains = ["yahoo", "hotmail", "mail", "live", "outlook"]
+  var secondLevelDomains = ["yahoo", "hotmail", "mail", "live", "outlook"]
 
-var $email = $('[type="email"]');
-var $hint = $(".email_hint");
+  var $email = $('[type="email"]');
 
-$email.on('blur', function() {
-  $(this).mailcheck({
-    domains: domains,
-    secondLevelDomains: secondLevelDomains,
-    suggested: function(element, suggestion) {
-      var text = "Did you mean <span class='suggestion'>" +
-        "<span class='address'>" + suggestion.address + "</span>" +
-        "@<a href='#' class='email_domain'>" + suggestion.domain +
-        "</a></span>?";
+  $email.on('blur', function() {
+    $(this).mailcheck({
+      domains: domains,
+      secondLevelDomains: secondLevelDomains,
+      suggested: function(element, suggestion) {
+        var text = "Did you mean <span class='suggestion'>" +
+          "<span class='address'>" + suggestion.address + "</span>" +
+          "@<a href='#' class='email_domain'>" + suggestion.domain +
+          "</a></span>?";
 
-      if (!$('.email_hint').length) {
-        $('<div class="email_hint">' + text + '</div>').insertAfter(element).fadeIn(150);
+        if (!$('.email_hint').length) {
+          $('<div class="email_hint">' + text + '</div>').insertAfter(element).fadeIn(150);
 
-      } else {
-        $(".email_hint").html(text);
+        } else {
+          $(".email_hint").html(text);
+        }
+      },
+      empty: function(element) {
+        $(".email_hint").html("");
       }
-    },
-    empty: function(element) {
-      $(".email_hint").html("");
-    }
+    });
   });
-});
 
 
-$(document).on('click', '.email_hint .suggestion a.email_domain', function() {
-  email_hint = $(this).parents(".email_hint");
-  email = $(email_hint).prevAll('input[type=email]:last');
+  $(document).on('click', '.email_hint .suggestion a.email_domain', function() {
+    email_hint = $(this).parents(".email_hint");
+    email = $(email_hint).prevAll('input[type=email]:last');
 
-  $(email).val($(".suggestion").first().text());
-  $(".email_hint").remove();
+    $(email).val($(".suggestion").first().text());
+    $(".email_hint").remove();
 
-  return false;
-});
+    return false;
+  });
+};
