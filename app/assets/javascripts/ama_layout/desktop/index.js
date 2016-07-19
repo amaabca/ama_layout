74dbd238912396a8294bff190f190e6d9025bb41
Jesse Doyle <jdoyle@ualberta.ca>
2016-07-19 15:16:48 -0600

    Design Refresh - Remove JS
    * Make the $(document).foundation() call included with
      ama/desktop javascript.

--- app/assets/javascripts/ama_layout/desktop/index.js
+++ app/assets/javascripts/ama_layout/desktop/index.js
@@ -1,5 +1,6 @@
 //= require ../ama_layout_namespace
 //= require ./foundation-custom
+//= require ./foundation-ready
 //= require ./foundation-equalizer-reflow
 //= require ./cookie.ready
 //= require ../mailcheck/index
