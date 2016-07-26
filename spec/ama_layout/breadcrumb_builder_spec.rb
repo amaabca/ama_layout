ecd0a922dbfab80fc047f5be48e28f24b7fbc75c
Jesse Doyle <jdoyle@ualberta.ca>
2016-07-26 09:13:25 -0600

    Design Refresh - Breadcrumbs
    * Make a few let calls more general in the breadcrumb_builder_spec
      as requested in PR comments.

--- spec/ama_layout/breadcrumb_builder_spec.rb
+++ spec/ama_layout/breadcrumb_builder_spec.rb
@@ -1,17 +1,13 @@
 describe AmaLayout::BreadcrumbBuilder do
   let(:view_context) { ActionView::Base.new }
+  let(:foo_crumb) { BreadcrumbsOnRails::Breadcrumbs::Element.new('Foo', '/foo') }
+  let(:bar_crumb) { BreadcrumbsOnRails::Breadcrumbs::Element.new('Bar', '/foo/bar') }
+  let(:crumbs) { [foo_crumb, bar_crumb] }
 
   describe '#render' do
     let(:builder) { AmaLayout::BreadcrumbBuilder.new(view_context, crumbs) }
 
     context 'no separator specified' do
-      let(:crumbs) do
-        [
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Foo', '/foo'),
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Bar', '/foo/bar')
-        ]
-      end
-
       it 'returns the breadcrumb HTML without any separator' do
         expect(builder.render).to eq('<li><a href="/foo">Foo</a></li><li><a href="/foo/bar">Bar</a></li>')
       end
@@ -19,12 +15,6 @@ describe AmaLayout::BreadcrumbBuilder do
 
     context 'with a separator of " > "' do
       let(:builder) { AmaLayout::BreadcrumbBuilder.new(view_context, crumbs, separator: ' > ') }
-      let(:crumbs) do
-        [
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Foo', '/foo'),
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Bar', '/foo/bar')
-        ]
-      end
 
       it 'returns the breadcrumb HTML with the proper separator' do
         expect(builder.render).to eq('<li><a href="/foo">Foo</a></li> > <li><a href="/foo/bar">Bar</a></li>')
@@ -32,12 +22,7 @@ describe AmaLayout::BreadcrumbBuilder do
     end
 
     context 'with a disabled element' do
-      let(:crumbs) do
-        [
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Foo', '/foo'),
-          BreadcrumbsOnRails::Breadcrumbs::Element.new('Bar', '/foo/bar', disabled: true)
-        ]
-      end
+      let(:bar_crumb) { BreadcrumbsOnRails::Breadcrumbs::Element.new('Bar', '/foo/bar', disabled: true) }
 
       it 'adds the appropriate disabled attributes to the element' do
         expect(builder.render).to match /breadcrumbs__link--disabled/
