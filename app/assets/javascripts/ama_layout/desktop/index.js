bb21ddbc14d9dfe8d92d97c8347f51c02adb38d1
Kathleen Robertson <kathleen.robertson@ama.ab.ca>
2016-12-16 09:14:07 -0700

    adding reflow back in

--- app/assets/javascripts/ama_layout/desktop/index.js
+++ app/assets/javascripts/ama_layout/desktop/index.js
@@ -1,6 +1,7 @@
 //= require ../ama_layout_namespace
 //= require ./foundation-custom
 //= require ./foundation-ready
+//= require ./foundation-equalizer-reflow
 //= require ./cookie.ready
 //= require ../mailcheck/index
 //= require ../real_date_picker
