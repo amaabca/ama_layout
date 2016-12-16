91c15ede5438db6377b07493611ccd3c7af0b903
Kathleen Robertson <kathleen.robertson@ama.ab.ca>
2016-12-16 09:12:49 -0700

    adding new colour variables and files for RR maps and ledgends

--- app/assets/javascripts/ama_layout/desktop/index.js
+++ app/assets/javascripts/ama_layout/desktop/index.js
@@ -1,7 +1,6 @@
 //= require ../ama_layout_namespace
 //= require ./foundation-custom
 //= require ./foundation-ready
-//= require ./foundation-equalizer-reflow
 //= require ./cookie.ready
 //= require ../mailcheck/index
 //= require ../real_date_picker
