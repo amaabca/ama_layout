8eb2c242067e7b7450afb9d5e4e294f04db13081
Jesse Doyle <jdoyle@ualberta.ca>
2017-06-20 17:17:35 -0600

    VSTS-438 - User Notifications
    * Add a `lifespan` attribute to all notifications. The notification
      will be automatically removed/garbage collected if the current
      date is past it's creation date plus the duration of seconds
      matching the lifespan.
    * Add Timecop to the development dependencies to test new lifespan
      behaviour.
    * Change the variable naming and spec descriptions to be more data
      store agnostic (in case we change the underlying datastore).
    * Add `all` and `active` scope methods on `NotificationSet` to
      help differentiate between the two groups.
    * Add a `destroy!` method to `NotificationSet` so we can easily destroy
      user notifications when needed.
    * Prevent the creation of new notifications if there is already
      an existing dismissed notification. If this needs to change in
      the future, we can add an alternate value into the hash function.
    
    SEE: https://amaabca.visualstudio.com/membership_backlog/_workitems?id=438

--- lib/ama_layout/notification_set.rb
+++ lib/ama_layout/notification_set.rb
@@ -5,11 +5,12 @@ module AmaLayout
   # The raw serialization format is JSON as follows (keys are SHA256 hashes):
   #
   # {
-  #   "02ac263cea5660e9f9020cb46e93772ed7755f2a60c40ad8961d2a15c1f99e6f": {
+  #   "8ca9f850c18acc17643038b2341bee3ede8a24c0f3e92f56f2109ce49fdcb616": {
   #     "type": "notice",
   #     "header": "test",
   #     "content": "test",
   #     "created_at": "2017-06-19T11:26:57.730-06:00",
+  #     "lifespan": 31557600,
   #     "active": true,
   #     "version": "1.0.0"
   #   }
@@ -20,35 +21,49 @@ module AmaLayout
 
     attr_accessor :base, :data_store, :key
 
-    delegate :first, :last, :size, :[], :empty?, :any?, to: :notifications
+    delegate :first, :last, :size, :[], :empty?, :any?, to: :active
 
     def initialize(data_store, key)
       self.data_store = data_store
-      self.key = "users/#{key}"
-      fetch
+      self.key = key
+      self.base = fetch
+      clean!
     end
 
-    def each(&block)
-      notifications.each do |notification|
-        block.call(notification)
-      end
+    def active
+      all.select(&:active?)
+    end
+
+    def all
+      @all ||= normalize(base_notifications)
     end
 
     def create(args = {})
       args[:created_at] = Time.current
       args[:active] = true
       notification = Notification.new(args)
-      notifications.push(notification)
+      # previously dismissed notifications always take precendence
+      all.push(notification) unless base.key?(notification.digest)
       save
     end
 
+    def destroy!
+      data_store.delete(key) && reload!
+    end
+
+    def each(&block)
+      active.each do |notification|
+        block.call(notification)
+      end
+    end
+
     def find(digest)
-      notifications.find { |n| n.id == digest }
+      all.find { |n| n.id == digest }
     end
 
     def save
       data_store.transaction do |store|
-        normalized = normalize(notifications)
+        normalized = normalize(all)
         self.base = serialize(normalized)
         store.set(key, base.to_json)
       end
@@ -56,34 +71,38 @@ module AmaLayout
     end
 
     def inspect
-      "<#{self.class.name}>: #{notifications}"
+      "<#{self.class.name}>: #{all}"
     end
     alias_method :to_s, :inspect
 
     private
 
-    def notifications
-      @notifications ||= normalize(active_notifications)
+    def clean!
+      if dirty?
+        all.reject!(&:stale?)
+        save
+      end
+    end
+
+    def dirty?
+      all.any?(&:stale?)
     end
 
     def reload!
-      @notifications = nil
-      notifications
+      @all = nil
+      self.base = fetch
+      all
       self
     end
 
-    def active_notifications
+    def base_notifications
       base
         .inject([]) { |m, (k, v)| m << Notification.new(v.merge(id: k)) }
-        .select(&:active?)
     end
 
     def serialize(data)
       data.inject({}) do |hash, element|
-        # if there is already a dismissed notification, ignore it
-        if !hash.key?(element.digest)
-          hash[element.digest] = element.to_h
-        end
+        hash[element.digest] = element.to_h
         hash
       end
     end
@@ -95,11 +114,7 @@ module AmaLayout
 
     def fetch
       result = data_store.get(key)
-      if result.present?
-        self.base = build(result)
-      else
-        self.base = {}
-      end
+      result.present? ? build(result) : {}
     end
 
     def build(raw)
