e56697d927ccc6e93d9ed676b916a5c78e393a05
Jesse Doyle <jdoyle@ualberta.ca>
2017-06-20 17:17:35 -0600

    VSTS-438 - User Notifications
    * Add a `lifespan` attribute to all notifications. The notification
      will be automatically removed/garbage collected if the current
      date is past it's creation date plus the duration of seconds
      matching the lifespan.
    * Add Timecop to the development dependencies to test new lifespan
      behaviour.
    * Change the variable naming and spec descriptions to be more data
      store agnostic (in case we change the underlying datastore).
    * Add `all` and `active` scope methods on `NotificationSet` to
      help differentiate between the two groups.
    * Prevent the creation of new notifications if there is already
      an existing dismissed notification. If this needs to change in
      the future, we can add an alternate value into the hash function.
    
    SEE: https://amaabca.visualstudio.com/membership_backlog/_workitems?id=438

--- lib/ama_layout/notification.rb
+++ lib/ama_layout/notification.rb
@@ -1,11 +1,12 @@
 module AmaLayout
   class Notification
     TYPES = %i[notice warning alert].freeze
+    DEFAULT_LIFESPAN = 1.year.freeze
     FORMAT_VERSION = '1.0.0'.freeze
 
     # NOTE: The following attributes are designed to be immutable - you need
     # make a new instance to change them. The only mutable attribute is :active.
-    attr_reader :id, :type, :header, :content, :created_at, :version
+    attr_reader :id, :type, :header, :content, :created_at, :lifespan, :version
     attr_accessor :active
 
     def initialize(args = {})
@@ -15,6 +16,7 @@ module AmaLayout
       @header = args.fetch(:header)
       @content = args.fetch(:content)
       @created_at = parse_time(args.fetch(:created_at))
+      @lifespan = parse_duration(args.fetch(:lifespan, DEFAULT_LIFESPAN))
       @version = args.fetch(:version, FORMAT_VERSION)
       self.active = args.fetch(:active)
       invalid_type! if TYPES.exclude?(type)
@@ -34,21 +36,30 @@ module AmaLayout
 
     def dismiss!
       self.active = false
-      true
+      dismissed?
     end
 
     def digest
-      Digest::SHA256.hexdigest(type.to_s + header + content + version)
+      Digest::SHA256.hexdigest(
+        "#{type}#{header}#{content}#{lifespan.to_i}#{version}"
+      )
+    end
+
+    def stale?
+      Time.current > created_at + lifespan
     end
 
     def to_h
+      # NOTE: We want the following keys to be strings to provide
+      # consistency with the underlying data store.
       {
-        type: type,
-        header: header,
-        content: content,
-        created_at: created_at,
-        active: active,
-        version: version
+        'type' => type.to_s,
+        'header' => header,
+        'content' => content,
+        'created_at' => created_at.iso8601,
+        'active' => active,
+        'lifespan' => lifespan.to_i,
+        'version' => version
       }
     end
 
@@ -60,6 +71,15 @@ module AmaLayout
 
     def parse_time(time)
       time.is_a?(String) ? Time.zone.parse(time) : time
+    end
+
+
+    def parse_duration(duration)
+      if duration.is_a?(ActiveSupport::Duration)
+        duration
+      else
+        duration.public_send(:seconds)
+      end
     end
   end
 end
--- spec/ama_layout/notification_set_spec.rb
+++ spec/ama_layout/notification_set_spec.rb
@@ -1,5 +1,5 @@
 describe AmaLayout::NotificationSet do
-  let(:redis) do
+  let(:store) do
     AmaLayout::Notifications::RedisStore.new(
       db: 4,
       namespace: 'test_notifications',
@@ -7,44 +7,80 @@ describe AmaLayout::NotificationSet do
     )
   end
   let(:key) { 1 }
-  let(:redis_key) { "users/#{key}" }
+  let(:duration) { AmaLayout::Notification::DEFAULT_LIFESPAN.to_i }
+  let(:store_key) { key.to_s }
   let(:json) do
     <<-JSON
     {
-      "02ac263cea5660e9f9020cb46e93772ed7755f2a60c40ad8961d2a15c1f99e6f": {
-      "type": "notice",
-      "header": "test",
-      "content": "test",
-      "created_at": "2017-06-19T11:26:57.730-06:00",
-      "active": true,
-      "version": "1.0.0"
+      "8ca9f850c18acc17643038b2341bee3ede8a24c0f3e92f56f2109ce49fdcb616": {
+        "type": "notice",
+        "header": "test",
+        "content": "test",
+        "created_at": "2017-06-19T06:00:00.000Z",
+        "active": true,
+        "lifespan": #{duration},
+        "version": "1.0.0"
+      }
+    }
+    JSON
+  end
+  let(:stale_json) do
+    <<-JSON
+    {
+      "d3c2bc71904100674325791b371db7446097f956ea76a304e787abd5f2588665": {
+        "type": "notice",
+        "header": "stale",
+        "content": "stale",
+        "created_at": "2012-06-19T06:00:00.000Z",
+        "active": true,
+        "lifespan": #{duration},
+        "version": "1.0.0"
       }
     }
     JSON
   end
 
-  subject { described_class.new(redis, key) }
+  subject { described_class.new(store, key) }
 
   around(:each) do |example|
-    redis.clear
-    example.run
-    redis.clear
+    Timecop.freeze(Time.zone.local(2017, 6, 19)) do
+      store.clear
+      example.run
+      store.clear
+    end
   end
 
   describe '#intialize' do
-    context 'with valid JSON in redis' do
+    context 'with valid JSON in data store' do
       before(:each) do
-        redis.set(redis_key, json)
+        store.set(store_key, notification)
       end
 
-      it 'fetches the notifications' do
-        expect(subject.size).to eq(1)
+      context 'without stale notifications in the data store' do
+        let(:notification) { json }
+
+        it 'fetches the notifications' do
+          expect(subject.size).to eq(1)
+        end
+      end
+
+      context 'with stale notifications in the data store' do
+        let(:notification) { stale_json }
+
+        it 'returns an empty set' do
+          expect(subject).to be_empty
+        end
+
+        it 'cleans out stale notifications from the data store' do
+          subject
+          expect(store.get(store_key)).to eq('{}')
+        end
       end
     end
 
-    context 'with invalid JSON in redis' do
+    context 'with invalid JSON in data store' do
       before(:each) do
-        redis.set(redis_key, '{"invalid_json":')
+        store.set(store_key, '{"invalid_json":')
       end
 
       it 'logs to Rails logger' do
@@ -52,9 +88,9 @@ describe AmaLayout::NotificationSet do
         subject
       end
 
-      it 'deletes the key in redis' do
+      it 'deletes the key in data store' do
         subject
-        expect(redis.get(redis_key)).to be nil
+        expect(store.get(store_key)).to be nil
       end
 
       it 'returns an empty set' do
@@ -66,7 +102,7 @@ describe AmaLayout::NotificationSet do
       end
     end
 
-    context 'with no entry in redis' do
+    context 'with no entry in data store' do
       it 'returns an empty set' do
         expect(subject).to be_empty
       end
@@ -75,7 +111,7 @@ describe AmaLayout::NotificationSet do
 
   describe '#each' do
     before(:each) do
-      redis.set(redis_key, json)
+      store.set(store_key, json)
     end
 
     it 'iterates the array' do
@@ -97,9 +133,28 @@ describe AmaLayout::NotificationSet do
       expect(subject.size).to eq(1)
     end
 
-    it 'saves a notification in redis' do
+    it 'saves a notification in data store' do
       subject.create(header: 'test', content: 'test')
-      expect(redis.get(redis_key)).to be_a(String)
+      expect(store.get(store_key)).to be_a(String)
+    end
+
+    context 'when the same notification exists but is dismissed' do
+      before(:each) do
+        store.set(store_key, json)
+        subject.first.dismiss!
+        subject.save
+        subject.create(header: 'test', content: 'test')
+      end
+
+      it 'does not overwrite the notification' do
+        expect(subject).to be_empty # we have only non-active notifications
+      end
+
+      it 'still has the dismissed notification in the data store' do
+        data = JSON.parse(store.get(store_key))
+        notification = data.values.first
+        expect(data.values.first['active']).to be false
+      end
     end
   end
 
@@ -141,8 +196,29 @@ describe AmaLayout::NotificationSet do
   end
 
   describe '#inspect' do
-    it 'returns a stringifed instance' do
+    it 'returns a stringified instance' do
       expect(subject.inspect).to eq('<AmaLayout::NotificationSet>: []')
+    end
+  end
+
+  context 'scoping' do
+    before(:each) do
+      subject.create(header: 'test', content: 'test')
+      subject.create(header: 'inactive', content: 'inactive')
+      subject.last.dismiss!
+      subject.save
+    end
+
+    describe '#all' do
+      it 'returns both active and inactive notifications' do
+        expect(subject.all.size).to eq(2)
+      end
+    end
+
+    describe '#active' do
+      it 'returns only active notifications' do
+        expect(subject.active.size).to eq(1)
+      end
     end
   end
 end
