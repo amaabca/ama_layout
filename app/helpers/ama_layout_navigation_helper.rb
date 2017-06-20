c8743c849c100e81b3b2d5aba71071a5719faf7d
Zoie Carnegie <zoie.carnegie@gmail.com>
2017-06-20 16:20:32 -0600

    Updated helper logic, updated/added tests, updated navigation

--- app/helpers/ama_layout_navigation_helper.rb
+++ app/helpers/ama_layout_navigation_helper.rb
@@ -3,10 +3,10 @@ module AmaLayoutNavigationHelper
   def navigation
     return AmaLayout::Navigation.non_member unless member?
     case
-    when in_renewal?
-      AmaLayout::Navigation.member_in_renewal
     when in_renewal_late?
       AmaLayout::Navigation.member_in_renewal_late
+    when in_renewal?
+      AmaLayout::Navigation.member_in_renewal
     else
       AmaLayout::Navigation.member
     end
