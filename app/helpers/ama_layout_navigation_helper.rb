2fbdb4e11fd04a7c9626e95115b911c266a5782b
Zoie Carnegie <zoie.carnegie@gmail.com>
2017-06-22 11:07:37 -0600

    Add tests for navigation helper module

--- app/helpers/ama_layout_navigation_helper.rb
+++ app/helpers/ama_layout_navigation_helper.rb
@@ -3,6 +3,8 @@ module AmaLayoutNavigationHelper
   def navigation
     return AmaLayout::Navigation.non_member unless member?
     case
+    when has_outstanding_balance?
+      AmaLayout::Navigation.member_with_outstanding_balance
     when in_renewal_late?
       AmaLayout::Navigation.member_in_renewal_late
     when in_renewal?
@@ -14,11 +16,19 @@ module AmaLayoutNavigationHelper
 
   private
 
+  def member?
+    member
+  end
+
   def in_renewal?
     in_renewal
   end
 
   def in_renewal_late?
     status == "AL"
+  end
+
+  def has_outstanding_balance?
+    has_outstanding_balance
   end
 end
