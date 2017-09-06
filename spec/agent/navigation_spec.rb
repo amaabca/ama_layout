ff0375e55b09d86e9145c680f2879dd6e6d73275
Jonathan Weyermann <jonathan.weyermann@ama.ab.ca>
2017-09-06 13:04:36 -0600

    Fixed tests for travis, added driver ed agent page

--- spec/agent/navigation_spec.rb
+++ spec/agent/navigation_spec.rb
@@ -1,4 +1,12 @@
 describe AmaLayout::Agent::Navigation do
+  let(:pos_site) { "http://pos.waffles.ca"}
+  let(:driver_ed_agent_site) { "https://driver_ed_agent_site.waffles.com" }
+
+  before(:each) do
+    allow(Rails.configuration).to receive(:driver_ed_agent_site).and_return(driver_ed_agent_site)
+    allow(Rails.configuration).to receive(:pos_site).and_return(pos_site)
+  end
+
   describe "#nav_file_path" do
     let(:file_path) { File.join(Gem.loaded_specs["ama_layout"].full_gem_path, "lib", "ama_layout", "agent_navigation.yml") }
 
@@ -44,7 +52,7 @@ describe AmaLayout::Agent::Navigation do
       context "main nav" do
         it "return the main nav items" do
           expect(subject.items[0].text).to eq "Customer Lookup"
-          expect(subject.items[0].link).to eq ""
+          expect(subject.items[0].link).to eq "#{pos_site}/customer_lookup/new"
           expect(subject.items[0].icon).to eq ""
         end
       end
@@ -54,7 +62,7 @@ describe AmaLayout::Agent::Navigation do
       context "main nav" do
         it "return the main nav items" do
           expect(subject.items[1].text).to eq "Driver Education"
-          expect(subject.items[1].link).to eq ""
+          expect(subject.items[1].link).to eq "#{driver_ed_agent_site}"
           expect(subject.items[1].icon).to eq "fa-car"
         end
       end
@@ -64,10 +72,10 @@ describe AmaLayout::Agent::Navigation do
 
         it "return the subnav items" do
           expect(driver_education_subnav[0].text).to eq "Purchase Course"
-          expect(driver_education_subnav[0].link).to eq ""
+          expect(driver_education_subnav[0].link).to eq "#{driver_ed_agent_site}"
 
           expect(driver_education_subnav[1].text).to eq "Admin Tasks"
-          expect(driver_education_subnav[1].link).to eq ""
+          expect(driver_education_subnav[1].link).to eq "#{driver_ed_agent_site}"
         end
       end
     end
