910764d623e9510baac57a2249d775f3a02be178
Michael van den Beuken <michael.beuken@gmail.com>
2020-01-22 08:25:05 -0700

    * update combustion config * update factories

--- spec/factories/navigation.rb
+++ spec/factories/navigation.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :navigation, class: AmaLayout::Navigation do
-    current_url "http://waffleemporium.ca"
+    current_url { 'http://waffleemporium.ca' }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,35 +1,37 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user, class: Class.new(OpenStruct) do
-    in_renewal false
-    member_type 'P'
-    renew_type 'R'
-    status 'A'
-    has_outstanding_balance false
-    member? true
+    in_renewal { false }
+    member_type { 'P' }
+    renew_type { 'R' }
+    status { 'A' }
+    has_outstanding_balance { false }
+    member? { true }
 
     trait :non_member do
-      member? false
+      member? { false }
     end
 
     trait :with_accr do
-      renew_type 'A'
+      renew_type { 'A' }
     end
 
     trait :with_mpp do
-      renew_type 'M'
+      renew_type { 'M' }
     end
 
     trait :in_renewal do
-      in_renewal true
+      in_renewal { true }
     end
 
     trait :in_renewal_late do
-      in_renewal true
-      status 'AL'
+      in_renewal { true }
+      status { 'AL' }
     end
 
     trait :outstanding_balance do
-      has_outstanding_balance true
+      has_outstanding_balance { true }
     end
   end
 end
